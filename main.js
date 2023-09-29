3e51f8840b1fa50b65867d23404d23e57a530478
Ajay <2ajaythakre@gmail.com>
2023-09-29 22:20:39 +0530

    updated delete

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ const taskSheet = document.querySelector(".taskSheet");
 
 taskSheet.addEventListener("click", (e) =>{
     console.log(e.target.tagName);
-    console.log(e.target.id);
+    console.log(e.target.classList);
     if (e.target.tagName === "P"){
         e.target.classList.toggle("checked");
 
-    } else if (e.target.id === "del"){
+    } else if (e.target.classList[1] === "fa-trash"){
         e.target.parentElement.remove();
         
-    } else if (e.target.id === "update"){
+    } else if (e.target.classList === "update"){
         console.log("update");
 
     }
@@ -29,14 +29,24 @@ function addTask() {
         let update = document.createElement("i")
         update.classList.add("fa-solid");
         update.classList.add("fa-pen-to-square");
-        update.setAttribute("id", "update");
         task.appendChild(update);
         let del = document.createElement("i");
         del.classList.add("fa-solid");
         del.classList.add("fa-trash");
-        del.setAttribute("id", "del");
         task.appendChild(del);
         taskSheet.appendChild(task);
         inputBox.value = "";
     }
 }
+
+function saveData(){
+    localStorage.setItem("data", taskSheet.innerHTML);
+}
+
+function showTask(){
+    localStorage.getItem("data");
+    console.log(data);
+}
+
+showTask();
+
