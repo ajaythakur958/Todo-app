62892414f35bc17c9516aeba4cc5652c15a5ee0b
Ajay <2ajaythakre@gmail.com>
2023-10-21 16:59:53 +0530

    added ejs-layout for navbar

--- inventory_app/index.js
+++ inventory_app/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import productController from './src/controllers/products.controller.js';
 import path from 'path';
+import ejsLayout from 'express-ejs-layouts';
 
 const server = express();
+server.use(ejsLayout);
 
 // creating productController instance
 const productControllers = new productController();
@@ -12,6 +14,6 @@ server.set("views", path.join(path.resolve(),'src','views'))
 
 server.get('/', productControllers.getProducts)
 
-server.use(express.static('src/views'))
+server.use(express.static('src/views'));
 
 server.listen(3200, ()=>(console.log("server started at 3200")));
