d35711d0e04c9c791f1164d7880c269ca1cc765a
Ajay <2ajaythakre@gmail.com>
2023-10-21 15:05:42 +0530

    fixed & added module for content render in home page

--- inventory_app/index.js
+++ inventory_app/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import productController from './src/controllers/products.controller.js';
+import path from 'path';
 
 const server = express();
 
 // creating productController instance
 const productControllers = new productController();
 
+server.set("view engine", "ejs");
+server.set("views", path.join(path.resolve(),'src','views'))
 
 server.get('/', productControllers.getProducts)
 
--- inventory_app/src/controllers/products.controller.js
+++ inventory_app/src/controllers/products.controller.js
@@ -1,7 +1,11 @@
 import path from 'path';
-
+import productModel from '../models/products.model.js'
 export default class productController{
+    
     getProducts(req, res){
-        res.sendFile(path.join(path.resolve(),'src','views','products.html'))
+        let products = productModel.get();
+        // console.log(productModel.get());
+        // res.sendFile(path.join(path.resolve(),'src','views','products.html'))
+        res.render("products", {products:products})
     }
 }
