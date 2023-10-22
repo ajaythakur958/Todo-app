96870db17c1ddd76b9f44f8a71483603c0a1a604
Ajay <2ajaythakre@gmail.com>
2023-10-22 15:26:32 +0530

    added validation function to check valid data

--- inventory_app/src/controllers/products.controller.js
+++ inventory_app/src/controllers/products.controller.js
@@ -9,11 +9,29 @@ export default class productController{
     }
 
     getAddForm(req, res){
-        return res.render('new-product')
+        let errors =[];
+        return res.render('new-product', {errorMessage:errors[0]})
     }
 
     addNewProduct(req,res){
         console.log(req.body);
+        const {name, price, imgurl} = req.body; // validating if client have entered details valid or not 
+        let errors = [];
+        if(!name || name.trim() == ''){
+            errors.push('name is invalid')
+        }
+        if (!price || price < 1){
+            errors.push('price is invalid')
+        }
+        try {
+            const validurl = new URL(imgurl);
+        } catch{
+            errors.push('invalid URL')
+        }
+
+        if (errors.length > 0){  // sending response if error shows for invalid data from client
+            return res.render('new-product', {errorMessage:errors})
+        }
         productModel.add(req.body);
         let products = productModel.get();
         res.render('products', {products: products})
